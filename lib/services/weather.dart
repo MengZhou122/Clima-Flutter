0547e5173021c5bc11270a65ae5eec9ee0eb006e
Meng Zhou <zhoumeng122@gmail.com>
2020-07-13 16:10:27 -0700

    final

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,30 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+const apiKey = 'd9df5c47cced5b46a57b6582f132fa4c';
+const openMapWeatherURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+  Future getCityWeather(String cityName) async {
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openMapWeatherURL?q=$cityName&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    //Future objects for await related method
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openMapWeatherURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
